8d9872865989aa734507ef10d051707f0161fe52
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-06-22 17:22:56 +0600

    index file updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,8 @@
-import { createRoot } from 'react-dom';
 import SweetAlert from './SweetAlert';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SweetAlert
-      title="Hello!"
-      text="This is a sweet alert"
-      icon="success"
-      onClose={() => {}}
-    />
-  </React.StrictMode>
-);
+    <SweetAlert />
+  </React.StrictMode>,
+)
 
